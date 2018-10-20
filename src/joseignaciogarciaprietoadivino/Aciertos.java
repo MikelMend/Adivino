c67e80135ef564e5ab773905b8d44fdfa1776bdc
José Ignacio García Prieto <nachogarciaprieto@gmail.com>
2018-10-20 19:31:08 +0200

    Versión cambio.
    Ahora el nº aleatorio generado será entre 10 y 50

--- src/joseignaciogarciaprietoadivino/Aciertos.java
+++ src/joseignaciogarciaprietoadivino/Aciertos.java
@@ -4,15 +4,13 @@ package joseignaciogarciaprietoadivino;
 import java.util.Scanner;
 
 
-
-
 public class Aciertos {
     
-    
-
+   
     public static void main(String[] args) {
         int n = 0,cont = 0;
-        int x = (int) (500 *Math.random());
+        int x = (int) (Math.random() * 50) + 10;
+                                
         try ( //System.out.print(x);
                 Scanner sc = new Scanner(System.in)) {
             do {
